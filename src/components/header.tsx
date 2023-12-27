ddda95b6a3700ffeda5deb021ef555fe86a0a246
Leandro <atiladevelop@gmail.com>
2023-12-27 16:43:37 -0300

    chore: working on Header component

---
+++ src/components/header.tsx
@@ -0,0 +1,35 @@
+import Link from 'next/link';
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Input,
+  Button,
+  Avatar,
+} from '@nextui-org/react';
+import { auth } from '@/auth';
+
+export default async function Header() {
+  const session = await auth();
+
+  return (
+    <Navbar className='shadow mb-6'>
+      <NavbarBrand>
+        <Link href='/' className='font-bold'>
+          Discuss
+        </Link>
+      </NavbarBrand>
+      <NavbarContent justify='center'>
+        <NavbarItem>
+          <Input />
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify='end'>
+        <NavbarItem>
+          {session?.user ? <div>Signed In</div> : <div>Signed Out</div>}
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  );
+}
